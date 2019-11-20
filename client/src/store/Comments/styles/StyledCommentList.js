1cc1b79e8b4ce9715c6a826c3e1f37c44c2ce05d
unknown <sebastiank24@wp.pl>
2019-11-20 13:46:28 +0100

    change comment width window

--- client/src/store/Comments/styles/StyledCommentList.js
+++ client/src/store/Comments/styles/StyledCommentList.js
@@ -4,7 +4,7 @@ export const StyledCommentList = styled.div`
   .comments-list-box {
     float: left;
     padding: 0px 15px;
-    width: 460px;
+    width: 464px;
     height: 210px;
     overflow-y: scroll;
   }
