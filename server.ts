1cf10b17045e9f24335ad1b4f8743513b8321d11
SebaAlex12 <sebastiank24@wp.pl>
2021-01-25 18:02:22 +0100

    update project

--- server.ts
+++ server.ts
@@ -16,6 +16,19 @@ import fs = require("fs");
 
 const app: express.Application = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const bodyParserJson = bodyParser.json({ limit: "50mb" });
 const bodyParserUrlencoded = bodyParser.urlencoded({
   limit: "50mb",
