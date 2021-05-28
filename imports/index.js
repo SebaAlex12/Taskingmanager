082b5e305dd017af42261e89ef1d4872601a4a2b
SebaAlex12 <sebastiank24@wp.pl>
2021-05-28 12:58:52 +0200

    mysql imports from api

--- imports/index.js
+++ imports/index.js
@@ -9,19 +9,20 @@ const {
   fetchApiCategoriesById
 } = require("./api");
 
-const { insertProduct } = require("./mysql");
+const { insertProduct, insertCategory } = require("./mysql");
 
 module.exports = {
   getImports: async function(){
     const products = await fetchApiProducts();
     const categories = await fetchApiCategories();
-    const productsRangeBegin = 10;
-    const productsRangeEnd = 11;
-    const categoriesRangeBegin = 10;
-    const categoriesRangeEnd = 11;
+    const productsRangeBegin = 25;
+    const productsRangeEnd = 2680;
+    const categoriesRangeBegin = 0;
+    const categoriesRangeEnd = 0;
 
     if(categories){
       // console.log("categories",categories);
+      
       let counter = 0;
       for (const item of categories){
         if(counter >= categoriesRangeBegin && counter <= categoriesRangeEnd){
@@ -30,15 +31,23 @@ module.exports = {
           //get category details
           const categoryDetails = await fetchApiCategoriesById(item["id"]);
           if(categoryDetails){
-            console.log("category details",categoryDetails);
+
+            const data = {
+              id: categoryDetails["id"],
+              parentId: categoryDetails["1"],
+              name: categoryDetails["name"] 
+            }
+            // const response = await insertCategory(data);
           }
           console.log("counter",counter);
         }
         counter++;
       }
+      console.log("categories number",categories.length);
     }
   
     if(products){
+      
       let counter = 0;
         for (const item of products) {
               if(counter >= productsRangeBegin && counter <= productsRangeEnd){
@@ -47,39 +56,65 @@ module.exports = {
                 // get product details
                 const productDetails = await fetchApiProductDetailsById(item["id"]);
                 if(productDetails){
-                  console.log("product details",productDetails);
+                  // console.log("product details",productDetails);
                 }              
   
                 // get product characteristic data
                  const productCharacteristic = await fetchApiProductCharacteristicById(item["id"]);
                 if(productCharacteristic){
-                  console.log("product characteristic",productCharacteristic);
+                  // console.log("product characteristic",productCharacteristic);
                 }
   
                 // get product prices data
                 const productPrices = await fetchApiProductPricesById(item["id"]);
                 if(productPrices){
-                  console.log("product prices",productPrices);
+                  // console.log("product prices",productPrices);
                 }
   
                 //get product categories data
                 const productCategories = await fetchApiProductCategoriesById(item["id"]);
                 if(productCategories){
-                  console.log("product categories",productCategories);
+                  // console.log("product categories",productCategories);
                 }
   
                 //get product photos data
                 const productPhotos = await fetchApiProductPhotosById(item["id"]);
                 if(productPhotos){
-                  console.log("product photos",productPhotos);
+                  // console.log("product photos",productPhotos);
                 }
 
 
+                if(productDetails && productCharacteristic && productPrices && productCategories && productPhotos){
+                  
+                  const printers = productCharacteristic.filter(element => element.name == "Drukarki" && element)
+                  .map((element) => element.value)
+                  .join(", ");
+
+                  const descriptions = productCharacteristic.filter((element) => {
+                      if(element.name == "Opis krótki" || element.name == "Opis długi"){
+                        return element;
+                      }
+                  });
+                  
+                  const data = {
+                    id: productDetails["id"],
+                    categoryId: productCategories[0]["id"],
+                    name: productDetails["name"],
+                    description1: descriptions[0] ? descriptions[0]["value"] : "",
+                    description2: descriptions[1] ? descriptions[1]["value"] : "",
+                    price: productPrices["brutto"],
+                    photo: productPhotos[0] ? productPhotos[0]["file"] : "",
+                    tags: printers
+                  }
+                  // console.log("data",data);
+                  const response = await insertProduct(data);
+                }
   
                 console.log("counter",counter);
               }
               counter++;
          }
+         console.log("products number",products.length);
     }
   }
 }
--- imports/mysql.js
+++ imports/mysql.js
@@ -3,16 +3,74 @@ const mysql = require("mysql");
 const mysql_db = require("../config/db_config.js");
 const mysql_connect = mysql.createConnection(mysql_db);
 
+mysql_connect.connect(function(err) {
+    if (err) throw err;
+});
+
 module.exports = {
-    insertProduct: async function(product){
-        //  insert products to mysql database
-        mysql_connect.connect(function(err) {
-            if (err) throw err;
-            const sql = "INSERT INTO produkty (id_kategorii, nazwa) VALUES (3, 'Blue Village 1')";
+    insertProduct: async function({id,categoryId,name,description1,description2,price,photo,tags}){
+        //  insert product to mysql database
+
+        let fileName = name.replace(" ","-");
+        fileName = fileName.replace(" ","-");
+
+
+            const sql = `INSERT INTO produkty (
+                id_kategorii, 
+                nazwa,
+                nazwa_pliku,
+                opis,
+                opis2,
+                nr,
+                widocznosc,
+                widoczny_glowna,
+                cena,
+                img,
+                tagi
+            ) VALUES (
+                ${categoryId}, 
+                "${name}",
+                "${fileName}",
+                "${description1}",
+                "${description2}",
+                ${id},
+                1,
+                0,
+                ${price},
+                "${photo}",
+                "${tags}"
+            )`;
+            // console.log("sql",sql);
             mysql_connect.query(sql, function (err, result) {
                 if (err) throw err;
                 console.log("1 record inserted, ID: " + result.insertId);
             });
-        });
+    },
+    insertCategory: async function({id,parentId,name}){
+             //  insert category to mysql database
+
+            parentId = parentId == 1 || parentId == null ? -1 : parentId;
+            const fileName = name;
+            const deepNested = parentId == 1 ? -1 : 0;
+
+                const sql = `INSERT INTO kategorie (
+                    id, parent, nazwa, nazwa_pliku, widocznosc, nr, g, title, description, keywords
+                    ) VALUES (
+                        ${id},
+                        ${parentId},
+                        "${name}",
+                        "${fileName}",
+                        1,
+                        ${id},
+                        ${deepNested},
+                        "${name}",
+                        "${name}",
+                        "${name}"
+                    )`;
+                  //  console.log("sql",sql);
+                mysql_connect.query(sql, function (err, result) {
+                    if (err) throw err;
+                    console.log("1 record inserted, ID: " + result.insertId);
+                });  
     }
 }
