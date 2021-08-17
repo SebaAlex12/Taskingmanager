9272efbc98f97480d8e159627e593a987f0ddb13
BlumoMariusz <roboczy@katalog-pozycjonowanie.pl>
2021-08-17 07:40:44 +0200

    update
    update

--- imports/mysql_wordpress/index.js
+++ imports/mysql_wordpress/index.js
@@ -121,8 +121,8 @@ module.exports = {
                                     
                             if(termCategoryIds){
                                 termCategoryIds = termCategoryIds.flat(1);
-                                console.log("xxxxxxxxxxxxxxxxxxxxxxxxx");
-                                console.log("product id:",product.id);
+                                // console.log("xxxxxxxxxxxxxxxxxxxxxxxxx");
+                                // console.log("product id:",product.id);
                                 // console.log("termCategoryIds",termCategoryIds);
                                 try{
                                         const termCategoriesLoop = async() => {
